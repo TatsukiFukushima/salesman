1cfd9a58a910cdc923ccbb7a9ba6537ce2e48ba7
TatsukiFukushima <tatsuki2222net@gmail.com>
2020-02-03 12:04:05 +0900

    varを極力撲滅

--- salesman.js
+++ salesman.js
@@ -1,20 +1,21 @@
+const n = 15;
+const nMinus = n-1;
+let points = [];
+let distances = [];
+let results = Array(nMinus).fill(0);
+let minDistance = 1000000;
+
 function setup() {
-  points = [];
-  n = 15;
-  nMinus = n-1;
-  distances = [];
-  results = Array(nMinus).fill(0);
-  minDistance = 1000000;
   // 点を配置
-  for (var i = 0; i < n; i++) {
-    var p = [Math.random() * windowWidth, Math.random() * windowHeight];
+  for (let i = 0; i < n; i++) {
+    let p = [Math.random() * windowWidth, Math.random() * windowHeight];
     points[i] = p;
   }
 
   // 各点ごとの距離を計算
-  for (var i = 0; i < n; i++) {
-    var distance = [];
-    for (var j = 0; j < n; j++) {
+  for (let i = 0; i < n; i++) {
+    let distance = [];
+    for (let j = 0; j < n; j++) {
       if (i == j) {
         distance[j] = 0;
       } else {
@@ -24,19 +25,21 @@ function setup() {
     distances[i] = distance;
   }
 
+  calcStartTime = millis();
   // ルートを探索。最初のみsetupに書く。
-  var route = Array(nMinus).fill(0);
-  var isAdded = Array(nMinus).fill(false);
-  for(var addNumber = 0; addNumber < nMinus; addNumber++) {
+  let route = Array(nMinus).fill(0);
+  let isAdded = Array(nMinus).fill(false);
+  for(let addNumber = 0; addNumber < nMinus; addNumber++) {
     isAdded[addNumber] = true;
     route[0] = addNumber;
-    calcRoute(1, route, isAdded, distances[nMinus][addNumber]);
+    calcRoute(0, route, isAdded, distances[nMinus][addNumber]);
     isAdded[addNumber] = false;
   }
 
   createCanvas(windowWidth, windowHeight);
   background(0);
   startTime = millis();
+  console.log((startTime-calcStartTime)/1000);
 }
 
 function draw() {
@@ -49,24 +52,24 @@ function calcRoute(i, route, isAdded, currentDistance) {
   if (currentDistance > minDistance) {
     return
   }
-  if (i == nMinus) {
-    if (currentDistance + distances[route[i-1]][i] < minDistance) {
-      for (var j=0; j<nMinus; j++) { // 値渡しをしたいため
+  if (i+1 == nMinus) {
+    if (currentDistance + distances[route[i]][nMinus] < minDistance) {
+      for (let j=0; j<nMinus; j++) { // 値渡しをしたいため
         results[j] = route[j];
       }
-      minDistance = currentDistance + distances[route[i-1]][i];
+      minDistance = currentDistance + distances[route[i]][nMinus];
     }
   } else {
-    for(var addNumber = 0; addNumber < nMinus; addNumber++) {
+    for(let addNumber = 0; addNumber < nMinus; addNumber++) {
       if (isAdded[addNumber] || isCross(i, route, addNumber)) {
 			  continue
 		  }
-      var nextDistance = currentDistance+distances[route[i-1]][addNumber]
+      let nextDistance = currentDistance+distances[route[i]][addNumber]
       if (nextDistance > minDistance) {
         continue
       }
       isAdded[addNumber] = true;
-		  route[i] = addNumber;
+		  route[i+1] = addNumber;
       calcRoute(i+1, route, isAdded, nextDistance);
 		  isAdded[addNumber] = false;
     }
@@ -75,18 +78,17 @@ function calcRoute(i, route, isAdded, currentDistance) {
 
 // drawRoute ルートを描画する p:ポイント r:ルート
 function drawRoute(p, r) {
-  for (var i = 0; i < n; i++) {
+  for (let i = 0; i < n; i++) {
     noStroke();
     fill(255);
     ellipse(p[i][0], p[i][1], 10, 10);
   }
 
-  const now = millis();
-  if (startTime+5000 < now) {
+  if (startTime+5000 < millis()) {
     strokeWeight(1);
     stroke(255, 3);
     line(p[nMinus][0], p[nMinus][1], p[r[0]][0], p[r[0]][1]);
-    for (var i = 0; i < n-2; i++) {
+    for (let i = 0; i < n-2; i++) {
       line(p[r[i]][0], p[r[i]][1], p[r[i+1]][0], p[r[i+1]][1]);
     }
     line(p[r[n-2]][0], p[r[n-2]][1], p[nMinus][0], p[nMinus][1]);
@@ -95,17 +97,17 @@ function drawRoute(p, r) {
 
 // isCross 線が交差するかのチェック。ベクトルを使っている。
 function isCross(i, route, addNumber) {
-  if (i < 2) {
+  if (i == 0) {
     return false;
   } else {
-    var newPointX2 = points[addNumber][0];
-    var newPointY2 = points[addNumber][1];
-    var newPointX1 = points[route[i-1]][0];
-    var newPointY1 = points[route[i-1]][1];
+    const newPointX2 = points[addNumber][0];
+    const newPointY2 = points[addNumber][1];
+    const newPointX1 = points[route[i]][0];
+    const newPointY1 = points[route[i]][1];
 
-    for (var j=0; j<i; j++) {
-      var oldPointX2 = points[route[j]][0];
-      var oldPointY2 = points[route[j]][1];
+    for (let j=0; j<=i; j++) {
+      const oldPointX2 = points[route[j]][0];
+      const oldPointY2 = points[route[j]][1];
       if (j == 0) {
         var oldPointX1 = points[nMinus][0];
         var oldPointY1 = points[nMinus][1];
@@ -114,15 +116,15 @@ function isCross(i, route, addNumber) {
         var oldPointY1 = points[route[j-1]][1];
       }
 
-      var v1X = oldPointX2-newPointX1;
-      var v1Y = oldPointY2-newPointY1;
-      var v2X = oldPointX1-newPointX1;
-      var v2Y = oldPointY1-newPointY1;
-      var v3X = newPointX2-newPointX1;
-      var v3Y = newPointY2-newPointY1;
-      var tUp = (v2Y*v3X-v2X*v3Y);
-      var uUp = (v1X*v3Y-v1Y*v3X);
-      var paramsDown = v1X*v2Y-v1Y*v2X;
+      const v1X = oldPointX2-newPointX1;
+      const v1Y = oldPointY2-newPointY1;
+      const v2X = oldPointX1-newPointX1;
+      const v2Y = oldPointY1-newPointY1;
+      const v3X = newPointX2-newPointX1;
+      const v3Y = newPointY2-newPointY1;
+      const tUp = (v2Y*v3X-v2X*v3Y);
+      const uUp = (v1X*v3Y-v1Y*v3X);
+      const paramsDown = v1X*v2Y-v1Y*v2X;
 
       // なるべく早く判断できるように調整
       if (tUp*uUp>0) {
