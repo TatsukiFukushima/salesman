a9eb9ec188e526db9eba4674f582c441c775627d
TatsukiFukushima <tatsuki2222net@gmail.com>
2020-02-04 16:23:30 +0900

    最後の点でも場合分け

--- salesman.js
+++ salesman.js
@@ -1,5 +1,6 @@
 const n = 15;
 const nMinus = n-1;
+const nMinus2 = n-2;
 let points = [];
 let distances = [];
 let vectors = [];
@@ -34,10 +35,17 @@ function setup() {
   // ルートを探索。最初のみsetupに書く。
   let route = Array(nMinus).fill(0);
   let isAdded = Array(nMinus).fill(false);
-  for(let addNumber = 0; addNumber < nMinus; addNumber++) {
+  for(let addNumber = 0; addNumber < nMinus2; addNumber++) {
+    let currentDistance = distances[nMinus][addNumber]
     isAdded[addNumber] = true;
     route[0] = addNumber;
-    calcRoute(0, route, isAdded, distances[nMinus][addNumber]);
+    // 最後の点で場合分け。重複を防ぐため。
+    for(let lastNumber = addNumber+1; lastNumber < nMinus; lastNumber++) {
+      isAdded[lastNumber] = true;
+      route[nMinus2] = lastNumber;
+      calcRoute(0, route, isAdded, currentDistance+distances[nMinus][lastNumber]);
+      isAdded[lastNumber] = false;
+    }
     isAdded[addNumber] = false;
   }
 
@@ -52,17 +60,17 @@ function draw() {
   drawRoute(points, results);
 }
 
-// calcRoute ルートを探索 スタートはn-1の点
+// calcRoute ルートを探索 スタートはn-1の点 i=0の時には既に３点決まっていることに注意。
 function calcRoute(i, route, isAdded, currentDistance) {
   if (currentDistance > minDistance) {
     return
   }
-  if (i+1 == nMinus) {
-    if (currentDistance + distances[route[i]][nMinus] < minDistance) {
+  if (i+2 == nMinus) {
+    if (currentDistance + distances[route[i]][route[nMinus2]] < minDistance) {
       for (let j=0; j<nMinus; j++) { // 値渡しをしたいため
         results[j] = route[j];
       }
-      minDistance = currentDistance + distances[route[i]][nMinus];
+      minDistance = currentDistance + distances[route[i]][route[nMinus2]];
     }
   } else {
     for(let addNumber = 0; addNumber < nMinus; addNumber++) {
@@ -102,33 +110,34 @@ function drawRoute(p, r) {
 
 // isCross 線が交差するかのチェック。ベクトルを使っている。
 function isCross(i, route, addNumber) {
-  if (i == 0) {
-    return false;
-  } else {
-    const vector = vectors[route[i]];
+  const vector = vectors[route[i]];
+  const v3 = vector[addNumber];
 
-    for (let j=0; j<i; j++) {
-      if (j == 0) {
-        var v1 = vector[nMinus];
+  for (let j=-1; j<i; j++) {
+    if (j < 1) {
+      var v1 = vector[nMinus];
+      if (j == -1) {
+        var v2 = vector[route[nMinus2]];
       } else {
-        var v1 = vector[route[j-1]];
+        var v2 = vector[route[0]];
       }
+    } else {
+      var v1 = vector[route[j-1]];
+      var v2 = vector[route[j]];
+    }
 
-      const v2 = vector[route[j]];
-      const v3 = vector[addNumber];
-      const tUp = (v2[1]*v3[0]-v2[0]*v3[1]);
-      const uUp = (v1[0]*v3[1]-v1[1]*v3[0]);
-      const paramsDown = v1[0]*v2[1]-v1[1]*v2[0];
+    const tUp = (v2[1]*v3[0]-v2[0]*v3[1]);
+    const uUp = (v1[0]*v3[1]-v1[1]*v3[0]);
+    const paramsDown = v1[0]*v2[1]-v1[1]*v2[0];
 
-      // なるべく早く判断できるように調整
-      if (tUp*uUp>0) {
-        if (tUp+uUp>paramsDown) {
-          if (paramsDown>0) {
-            return true;
-          }
-        } else if (paramsDown<0) {
+    // なるべく早く判断できるように調整
+    if (tUp*uUp>0) {
+      if (tUp+uUp>paramsDown) {
+        if (paramsDown>0) {
           return true;
         }
+      } else if (paramsDown<0) {
+        return true;
       }
     }
   }
